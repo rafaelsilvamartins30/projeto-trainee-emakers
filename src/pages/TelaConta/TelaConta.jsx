ddbc75ac173b89e055e6ece52e6e3d25a66d2ca0
Rafael Silva Martins <rafael.sm008@gmail.com>
2025-01-04 02:53:11 -0300

    att conta

--- src/pages/TelaConta/TelaConta.jsx
+++ src/pages/TelaConta/TelaConta.jsx
@@ -11,31 +11,31 @@ const TelaConta = () => {
         <Cabecalho />
       </div>
       <div className='midconta'>
-        <div className='direitaconta'>
-          <div className='textdireita'>
-            <h1 className='titletextcontadireita'>Olá, nome usuario</h1>
-            <h1 className='textcontadireita'>Seu email é nomeusuario@email.com</h1>
-            <h1 className='textcontadireita'>Seu CPF é 123.456.789-00</h1>
-            <button className='buttoncontadireita'>
-              <h1 className='textbutoncontadireita'>Alterar Dados</h1>
+        <div className='esquerdaconta'>
+          <div className='textesquerda'>
+            <h1 className='titletextcontaesquerda'>Olá, nome usuario</h1>
+            <h1 className='textcontaesquerda'>Seu email é nomeusuario@email.com</h1>
+            <h1 className='textcontaesquerda'>Seu CPF é 123.456.789-00</h1>
+            <button className='buttoncontaesquerda'>
+              <h1 className='textbutoncontaesquerda'>Alterar Dados</h1>
               <i className='fas fa-pencil-alt'></i>
             </button>
-            <button className='buttoncontadireita'>
-              <h1 className='textbutoncontadireita'>Excluir Conta</h1>
+            <button className='buttoncontaesquerda'>
+              <h1 className='textbutoncontaesquerda'>Excluir Conta</h1>
               <i className='fas fa-trash'></i>
             </button>
-            <button className='buttoncontadireitaadm'><h1 className='textbutoncontadireita'>Tela do admin</h1></button>
+            <button className='buttoncontaesquerdaadm'><h1 className='textbutoncontaesquerda'>Tela do admin</h1></button>
           </div>
         </div>
-        <div className='esquerdaconta'>
-          <div className='titulocontaesquerda'>
-            <h1 className='titletextcontaesquerda'>Jogos adquiridos:</h1>
+        <div className='direitaconta'>
+          <div className='titulocontadireita'>
+            <h1 className='titletextcontadireita'>Jogos adquiridos:</h1>
           </div>
           <div className='gridconta'>
-            <div className='grid1conta'> <img className='contaimgesquerda' src="/src/imgs/bikiniconta.png" alt="" /> <h1 className='contatextesquerda' >100/100</h1> </div>
-            <div className='grid2conta'> <img className='contaimgesquerda' src="/src/imgs/marsconta.png" alt="" /> <h1 className='contatextesquerda' >95/100</h1> </div>
-            <div className='grid3conta'> <img className='contaimgesquerda' src="/src/imgs/godconta.png" alt="" /> <h1 className='contatextesquerda' >75/100</h1> </div>
-            <div className='grid4conta'> <img className='contaimgesquerda' src="/src/imgs/stranconta.png" alt="" /> <h1 className='contatextesquerda'>Adicionar Avaliação</h1> </div>
+            <div className='grid1conta'> <img className='contaimgdireita' src="/src/imgs/bikiniconta.png" alt="" /> <h1 className='contatextdireita' >100/100</h1> </div>
+            <div className='grid2conta'> <img className='contaimgdireita' src="/src/imgs/marsconta.png" alt="" /> <h1 className='contatextdireita' >95/100</h1> </div>
+            <div className='grid3conta'> <img className='contaimgdireita' src="/src/imgs/godconta.png" alt="" /> <h1 className='contatextdireita' >75/100</h1> </div>
+            <div className='grid4conta'> <img className='contaimgdireita' src="/src/imgs/stranconta.png" alt="" /> <h1 className='contatextdireita'>Adicionar Avaliação</h1> </div>
           </div>
           <div className='carrinhoiconconta'>
           <button className='carrinhoconta' type="submit"> 

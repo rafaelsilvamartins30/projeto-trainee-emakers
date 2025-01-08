02ead03ceb2a09e0aa00e3e490793a99184623d0
Rafael Silva Martins <rafael.sm008@gmail.com>
2025-01-08 02:27:48 -0300

    FINALIZANDO CARROSEL DE LANCAMENTO

--- src/pages/TelaPrincipal/Telaprincipal.jsx
+++ src/pages/TelaPrincipal/Telaprincipal.jsx
@@ -1,10 +1,36 @@
 import React from 'react' 
 import "./Telaprincipal.css"
 import Cabecalho from '../../components/Cabecalho/Cabecalho'
-import Rodape from '../../components/rodape/rodape'
+import Rodape from '../../components/rodape/rodape' 
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { SlArrowRight } from "react-icons/sl";
+import { SlArrowLeft } from "react-icons/sl";
+
+const SampleNextArrowCarrossel = ({ onClick }) => (
+  <button onClick={onClick} className='iconeAvancarSlideCarrossel'>
+    <SlArrowRight />
+  </button>
+);
+
+const SamplePrevArrowCarrossel = ({ onClick }) => (
+  <button onClick={onClick} className='iconeVoltarSlideCarrossel'>
+    < SlArrowLeft />
+  </button>
+);
+
+const SampleNextArrowCarrosselLancamento = ({ onClick }) => (
+  <button onClick={onClick} className='iconeAvancarSlideCarrosselLancamento'>
+    <SlArrowRight />
+  </button>
+);
+
+const SamplePrevArrowCarrosselLancamento = ({ onClick }) => (
+  <button onClick={onClick} className='iconeVoltarSlideCarrosselLancamento'>
+    < SlArrowLeft />
+  </button>
+);
 
 const Telaprincipal = () => {
   
@@ -14,8 +40,30 @@ const Telaprincipal = () => {
       speed: 500,
       slidesToShow: 5,
       slidesToScroll: 1,
-    } 
-
+      arrows: true,
+      nextArrow: <SampleNextArrowCarrosselLancamento/>,
+      prevArrow: <SamplePrevArrowCarrosselLancamento/>,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    };
   return (
     <div className='homegeral'>
 
@@ -29,39 +77,41 @@ const Telaprincipal = () => {
         </div>
 
         <div className='carroselbaixo'>
-        <h1 className='titlelancamentos'>Lançamentos</h1>
-          <Slider className='carrossellancamentos' {...settingsLancamentos}>
-              <div className="itemlancamento1">
-                <img className='imglancamento' src="src/imgs/bikini.png" alt="" />
-                <div className='infolancamento'>
-                  <p className='precolancamento'>R$ 299,00</p>
+          <h1 className='titlelancamentos'>Lançamentos</h1>
+          <div className='carrossellancamentos'>
+            <Slider className='sliderlancamentos' {...settingsLancamentos}>
+                <div className="itemlancamento">
+                  <div className='infolancamento'>
+                    <h1 className='precolancamento'>R$ 299,00</h1>
+                  </div>
+                  <img className='imglancamento' src="src/imgs/bikini.png" alt="" />
                 </div>
-              </div>
-              <div className="itemlancamento2">
-                <img className='imglancamento' src="src/imgs/deliver.png" alt="" />
-                <div className='infolancamento'>
-                  <p className='precolancamento'>R$ 299,00</p>
+                <div className="itemlancamento">
+                  <div className='infolancamento'>
+                    <h1 className='precolancamento'>R$ 299,00</h1>
+                  </div>
+                  <img className='imglancamento' src="src/imgs/deliver.png" alt="" />
                 </div>
-              </div>
-              <div className="itemlancamento3">
-                <img className='imglancamento' src="src/imgs/onepiece.png" alt="" />
-                <div className='infolancamento'>
-                  <p className='precolancamento'>R$ 299,00</p>
+                <div className="itemlancamento">
+                  <div className='infolancamento'>
+                    <h1 className='precolancamento'>R$ 299,00</h1>
+                  </div>
+                  <img className='imglancamento' src="src/imgs/onepiece.png" alt="" />
                 </div>
-              </div>
-              <div className="itemlancamento4">
-                <img className='imglancamento' src="src/imgs/deathstranding.png" alt="" />
-                <div className='infolancamento'>
-                  <p className='precolancamento'>R$ 299,00</p>
+                <div className="itemlancamento">
+                  <div className='infolancamento'>
+                    <h1 className='precolancamento'>R$ 299,00</h1>
+                  </div>
+                  <img className='imglancamento' src="src/imgs/deathstranding.png" alt="" />
                 </div>
-              </div>
-              <div className="itemlancamento5">
-                <img className='imglancamento' src="src/imgs/parish.png" alt="" />
-                <div className='infolancamento'>
-                  <p className='precolancamento'>R$ 299,00</p>
+                <div className="itemlancamento">
+                  <div className='infolancamento'>
+                    <h1 className='precolancamento'>R$ 299,00</h1>
+                  </div>
+                  <img className='imglancamento' src="src/imgs/parish.png" alt="" />
                 </div>
-              </div>
-          </Slider>
+            </Slider>
+          </div>
         </div>
       </div>
 
